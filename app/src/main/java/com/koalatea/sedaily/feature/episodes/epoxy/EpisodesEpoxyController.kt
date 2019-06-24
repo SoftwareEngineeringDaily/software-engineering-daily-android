d78617b291adba0129d40797e08f4a20716fd73b
Mostafa Gazar <mmegazar@gmail.com>
2019-06-25 05:46:07 +1200

    Add itemDiffCallback to EpisodesEpoxyController

--- app/src/main/java/com/koalatea/sedaily/feature/episodes/epoxy/EpisodesEpoxyController.kt
+++ app/src/main/java/com/koalatea/sedaily/feature/episodes/epoxy/EpisodesEpoxyController.kt
@@ -1,5 +1,6 @@
 package com.koalatea.sedaily.feature.episodes.epoxy
 
+import androidx.recyclerview.widget.DiffUtil
 import com.airbnb.epoxy.EpoxyModel
 import com.airbnb.epoxy.paging.PagedListEpoxyController
 import com.koalatea.sedaily.database.model.Episode
@@ -9,7 +10,13 @@ class EpisodesEpoxyController(
         private val commentClickListener: (episode: Episode) -> Unit,
         private val bookmarkClickListener: (episode: Episode) -> Unit,
         private val episodeClickListener: (episode: Episode) -> Unit
-) : PagedListEpoxyController<Episode>() {
+) : PagedListEpoxyController<Episode>(
+        itemDiffCallback = object : DiffUtil.ItemCallback<Episode>() {
+            override fun areItemsTheSame(oldItem: Episode, newItem: Episode): Boolean = oldItem._id == newItem._id
+
+            override fun areContentsTheSame(oldItem: Episode, newItem: Episode): Boolean = oldItem == newItem
+        }
+) {
 
     override fun buildItemModel(currentPosition: Int, item: Episode?): EpoxyModel<*> {
         return if (item == null) {
