310205408e21756040fe383a94a9338f09360bb3
Mostafa Gazar <mmegazar@gmail.com>
2019-07-08 15:00:37 +1200

    Add screen analytics tracking

--- app/src/main/java/com/koalatea/sedaily/ui/fragment/BaseFragment.kt
+++ app/src/main/java/com/koalatea/sedaily/ui/fragment/BaseFragment.kt
@@ -1,11 +1,32 @@
 package com.koalatea.sedaily.ui.fragment
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.analytics.FirebaseAnalytics
 import com.koalatea.sedaily.R
+import com.koalatea.sedaily.analytics.AnalyticsConstants
 import com.koalatea.sedaily.network.Resource
+import org.koin.android.ext.android.inject
 
 abstract class BaseFragment : Fragment() {
+
+    private val firebaseAnalytics: FirebaseAnalytics by inject()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        firebaseAnalytics.setCurrentScreen(requireActivity(), this@BaseFragment::class.java.simpleName, null)
+
+//        if (savedInstanceState == null) {
+//            firebaseAnalytics.logEvent(AnalyticsConstants.EVENT_SCREEN_VIEW, Bundle().apply {
+//                putString(AnalyticsConstants.KEY_SCREEN_NAME, this@BaseFragment::class.java.simpleName)
+//            })
+//
+////            FirebaseAnalytics.Param.
+//        }
+    }
 
     protected fun acknowledgeGenericError()
             = Snackbar.make(requireView(), R.string.error_generic, Snackbar.LENGTH_SHORT).show()
